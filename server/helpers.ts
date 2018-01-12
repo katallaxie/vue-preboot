0d258be7be278bc7e057e65a5cd2bfb679baa721
Döll, Sebastian <sebastian.doell@axelspringer.de>
2018-01-12 23:04:54 +0100

    🐛 ~ serving static compressed

--- server/helpers.ts
+++ server/helpers.ts
@@ -1,12 +1,16 @@
 import * as Path from 'path'
 import * as express from 'express'
+import * as compress from 'express-static-gzip'
 
 export const isProd = process.env.NODE_ENV === 'production'
 export const resolve = file => Path.resolve(__dirname, file)
-export const serve = (path, cache) => express.static(resolve(path), {
-  // evalute times on prod
-  maxAge: cache && isProd ? 60 * 60 * 24 * 30 : 0
-})
+export const serve = (path, cache) => isProd
+  ? compress(resolve(path), {
+    // evalute times on prod
+    maxAge: cache && isProd ? 60 * 60 * 24 * 30 : 0,
+    enableBrotli: true
+  })
+  : express.static(resolve(path))
 
 export const createRenderer = (bundle, template, options) => {
   // https://github.com/vuejs/vue/blob/dev/packages/vue-server-renderer/README.md#why-use-bundlerenderer
