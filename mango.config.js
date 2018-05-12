c2c755f06ca754d207a6d77fc4018734e7996042
Döll, Sebastian <sebastian.doell@spring-media.de>
2018-05-12 10:23:16 +0200

    ~ adding mango config

---
+++ mango.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  serve: 'public',
+  bundle: 'public/vue-ssr-server-bundle.json',
+  manifest: 'public/vue-ssr-client-manifest.json',
+  template: 'public/index.html',
+  webpack: 'config/configs'
+}
